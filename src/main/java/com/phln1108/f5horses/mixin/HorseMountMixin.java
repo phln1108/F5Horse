afc3c09c00a258e1db2300704b7ab851a8bc6837
phln1108 <pedro.ph.henrique703@gmail.com>
2025-04-23 09:54:09 -0300

    refactoring code to support /ride command

--- src/main/java/com/phln1108/f5horses/mixin/HorseMountMixin.java
+++ src/main/java/com/phln1108/f5horses/mixin/HorseMountMixin.java
@@ -2,6 +2,7 @@ package com.phln1108.f5horses.mixin;
 
 import com.phln1108.f5horses.HorseMountCallback;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.passive.HorseEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ActionResult;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,12 +12,25 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
 public class HorseMountMixin {
-    @Inject(method = "startRiding(Lnet/minecraft/entity/Entity;)Z", at = @At("HEAD"), cancellable = true)
-    private void onMount(Entity entity, CallbackInfoReturnable<ActionResult> info) {
-        ActionResult result = HorseMountCallback.EVENT.invoker().interact((PlayerEntity) (Object) this);
+//    @Inject(method = "startRiding(Lnet/minecraft/entity/Entity;)Z", at = @At("TAIL"), cancellable = true)
+//    private void onMount(Entity entity, CallbackInfoReturnable<Boolean> info) {
+//        ActionResult result = HorseMountCallback.EVENT.invoker().interact((PlayerEntity) (Object) this);
+//
+//        if (result == ActionResult.FAIL) {
+//            info.setReturnValue(false);
+//        }
+//    }
 
-        if (result == ActionResult.FAIL) {
-            info.setReturnValue(result);
+    @Inject(method = "startRiding(Lnet/minecraft/entity/Entity;Z)Z", at = @At("TAIL"), cancellable = true)
+    private void onMountForced(Entity entity, boolean force, CallbackInfoReturnable<Boolean> info) {
+        if ((Object) this instanceof PlayerEntity player) {
+//        if (!((Object) this instanceof PlayerEntity player) || !(entity instanceof HorseEntity)) return;
+            // Call your event
+            ActionResult result = HorseMountCallback.EVENT.invoker().interact(player);
+
+            if (result == ActionResult.FAIL) {
+                info.setReturnValue(false);
+            }
         }
     }
 }
